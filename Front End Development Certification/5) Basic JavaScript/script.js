9ce23b7211e8800f53036a395d67a8d0c2c34d25
Dardan Demiri <dardan.ddemiri@gmail.com>
2018-02-04 23:24:13 +0100

    Task's done in FCC ( Basic JavaScript )

---
+++ Front End Development Certification/5) Basic JavaScript/script.js
@@ -0,0 +1,100 @@
+// Comment your JavaScript Code Complete
+//
+// Declare JavaScript Variables Complete
+//
+// Storing Values with the Assignment Operator Complete
+//
+// Initializing Variables with the Assignment Operator Complete
+//
+// Understanding Uninitialized Variables Complete
+//
+// Understanding Case Sensitivity in Variables Complete
+//
+// Add Two Numbers with JavaScript Complete
+//
+// Subtract One Number from Another with JavaScript Complete
+//
+// Multiply Two Numbers with JavaScript Complete
+//
+// Divide One Number by Another with JavaScript Complete
+//
+// Increment a Number with JavaScript Complete
+//
+// Decrement a Number with JavaScript Complete
+//
+// Create Decimal Numbers with JavaScript Complete
+//
+// Multiply Two Decimals with JavaScript Complete
+//
+// Divide one Decimal by Another with JavaScript Complete
+//
+// Finding a Remainder in JavaScript Complete
+//
+// Compound Assignment With Augmented Addition Complete
+//
+// Compound Assignment With Augmented Subtraction Complete
+//
+// Compound Assignment With Augmented Multiplication Complete
+//
+// Compound Assignment With Augmented Division Complete
+//
+// Convert Celsius to Fahrenheit Complete
+//
+// Declare String Variables Complete
+//
+// Escaping Literal Quotes in Strings Complete
+//
+// Quoting Strings with Single Quotes Complete
+//
+// Escape Sequences in Strings Complete
+//
+// Concatenating Strings with Plus Operator Complete
+//
+// Concatenating Strings with the Plus Equals Operator Complete
+//
+// Constructing Strings with Variables Complete
+//
+// Appending Variables to Strings Complete
+//
+// Find the Length of a String Complete
+//
+// Use Bracket Notation to Find the First Character in a String Complete
+//
+// Understand String Immutability Complete
+//
+// Use Bracket Notation to Find the Nth Character in a String Complete
+//
+// Use Bracket Notation to Find the Last Character in a String Complete
+//
+// Use Bracket Notation to Find the Nth-to-Last Character in a String Complete
+//
+// Word Blanks Complete
+//
+// Store Multiple Values in one Variable using JavaScript Arrays Complete
+//
+// Nest one Array within Another Array Complete
+//
+// Access Array Data with Indexes Complete
+//
+// Modify Array Data With Indexes Complete
+//
+// Access Multi-Dimensional Arrays With Indexes Complete
+//
+// Manipulate Arrays With push() Complete
+//
+// Manipulate Arrays With pop() Complete
+//
+// Manipulate Arrays With shift() Complete
+//
+// Manipulate Arrays With unshift() Complete
+//
+// Shopping List Complete
+//
+// Write Reusable JavaScript with Functions Complete
+//
+// Passing Values to Functions with Arguments Complete
+//
+// Global Scope and Functions Complete
+//
+// Local Scope and Functions Complete
+//
