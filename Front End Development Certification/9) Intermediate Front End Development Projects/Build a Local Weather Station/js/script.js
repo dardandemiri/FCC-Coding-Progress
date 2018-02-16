0237e572a38dcffc242d111aa80644b1697e71a3
Dardan Demiri <dardan.ddemiri@gmail.com>
2018-02-16 23:29:39 +0100

    Adding the finished JS file

--- Front End Development Certification/9) Intermediate Front End Development Projects/Build a Local Weather Station/js/script.js
+++ Front End Development Certification/9) Intermediate Front End Development Projects/Build a Local Weather Station/js/script.js
@@ -1,53 +1,197 @@
-$(document).ready(function () {
+var cities = [
+  "Venice",
+  "Hong+Kong",
+  "Istanbul",
+  "New+York",
+  "London",
+  "Paris",
+  "Cape+Town",
+  "Amsterdam",
+  "Beirut",
+  "Kyoto",
+  "Queenstown",
+  "Barcelona",
+  "Singapore",
+  "Havana",
+  "Florence",
+  "Sydney",
+  "Lisbon",
+  "Rio+de+Janeiro",
+  "Jaipur",
+  "Lucerne",
+  "Shanghai",
+  "San+Francisco",
+  "Rome",
+  "Bruges",
+  "Stockholm",
+  "Cartagena",
+  "Budapest",
+  "Prague",
+  "Edinburgh",
+  "Busan",
+  "Mexico+City",
+  "Charleston",
+  "Dubrovnik",
+  "Riga",
+  "Quito",
+  "Vienna",
+  "Quebec City",
+  "Jerusalem",
+  "Buenos+Aires",
+  "Isfahan",
+  "Seville",
+  "Chicago",
+  "Kiev",
+  "Hamburg",
+  "Krakow"
+];
+
+
+var tempValueC;
+var tempValueF;
+var latANDlon;
+var cityEntered;
+
+var searchURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var locationURL = "https://api.openweathermap.org/data/2.5/find?";
+var apiKey = "&APPID=f0c7f1a6071bf1c3e32c2e120091a56a&units=imperial";
+
+
+$(document).ready(function() {
   $(".searchArea").hide();
-  clickedSearch = true;
-});
+  $(".results").hide();
+  $(".results2").hide();
 
-
-
-var clickedSearch = false;
-$(".search").click(function() {
-
-  if(clickedSearch){
-    $(".searchArea").fadeIn(300);
-    clickedSearch = false;
-  }else{
-    $(".searchArea").fadeOut(300);
-    clickedSearch = true;
+  function tempConverter(temp) {
+    tempValueF = temp;
+    tempValueC = Math.round(5 / 9 * (temp - 32));
+    return tempValueC;
   }
-});
 
-$("#celcius").click(function(){
-  $("#celcius").addClass("active");
-  $("#farenheid").removeClass("active");
-});
-$("#farenheid").click(function(){
-  $("#farenheid").addClass("active");
-  $("#celcius").removeClass("active");
-});
+  clickedSearch = true;
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latANDlon =
+        "lat=" +
+        position.coords.latitude +
+        "&lon=" +
+        position.coords.longitude +
+        "&cnt=1";
+    });
+  }
 
+  var clickedSearch = true;
+  $(".search").click(function() {
+    if (clickedSearch) {
+      $(".searchArea").fadeIn(300);
+      clickedSearch = false;
+    } else {
+      $(".searchArea").fadeOut(300);
+      clickedSearch = true;
+    }
+  });
 
-var latitude;
-var longitude;
+  $("#celcius").click(function() {
+    $("#celcius").addClass("active");
+    $("#farenheid").removeClass("active");
+    $("#tempVal").text(tempValueC);
+  });
 
-$(".myLocation").click(function() {
+  $("#farenheid").click(function() {
+    $("#farenheid").addClass("active");
+    $("#celcius").removeClass("active");
+    $("#tempVal").text(tempValueF);
+  });
+
+  $(".randomWeather").click(function() {
     $(".searchArea").fadeOut(300);
     clickedSearch = true;
+    var randomNr = Math.round(Math.random() * cities.length);
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
+    function getWeather(locationURL) {
+      $.ajax({
+        url: searchURL + cities[randomNr] + apiKey,
+        success: function(result) {
+          var city = result.city.name;
+          var temp = tempConverter(result.list[0].main.temp);
+          var wind = result.list[0].wind.speed + " km/h";
+          var humidity = result.list[0].main.humidity;
+          var description = result.list[0].weather[0].description;
+
+          valUpdater(city, temp, wind, humidity, description);
+        }
       });
     }
+    getWeather(locationURL);
+    $(".results").show();
+    $(".results2").show();
+  });
 
 
 
-
-});
-
-$(".randomWeather").click(function() {
+  $(".myLocation").click(function() {
     $(".searchArea").fadeOut(300);
     clickedSearch = true;
+    $(".results").show();
+    $(".results2").show();
+
+    function getWeather(searchURL) {
+      $.ajax({
+        url: locationURL + latANDlon + apiKey,
+        success: function(result) {
+          var city = result.list[0].name;
+          var temp = tempConverter(result.list[0].main.temp);
+          var wind = result.list[0].wind.speed + " km/h";
+          var humidity = result.list[0].main.humidity;
+          var description = result.list[0].weather[0].description;
+
+          valUpdater(city, temp, wind, humidity, description);
+        }
+      });
+    }
+    getWeather(locationURL);
+  });
+
+  $("#searchBTN").click(function() {
+    cityEntered = $("#searchIn").val();
+
+    function getWeather(searchURL) {
+      $.ajax({
+        url: searchURL + cityEntered + apiKey,
+        success: function(result) {
+          var city = result.city.name;
+          var temp = tempConverter(result.list[0].main.temp);
+          var wind = result.list[0].wind.speed + " km/h";
+          var humidity = result.list[0].main.humidity;
+          var description = result.list[0].weather[0].description;
+          console.log(city);
+          valUpdater(city, temp, wind, humidity, description);
+        }
+      });
+    }
+    getWeather(searchURL);
+    $(".results").show();
+    $(".results2").show();
+  });
+
+  function valUpdater(city, temp, wind, humidity, description) {
+    $("#city").text(city);
+    $("#tempVal").text(temp);
+    $("#wind").text(wind);
+    $("#humidity").text(humidity);
+    $("#description").text(titleCase(description));
+  }
+
+  function titleCase(str) {
+    var strToArr = str.toLowerCase().split(" ");
+
+    var result = strToArr.map(function(val) {
+      return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+    });
+
+    return result.join(" ");
+  }
+
+
 });
