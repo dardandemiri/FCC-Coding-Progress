8c6207b00a61e2199350b84c85940c62c144c1ad
Dardan Demiri <dardan.ddemiri@gmail.com>
2018-03-05 23:18:12 +0100

    Just started with the JS by adding the chanels

--- Front End Development Certification/9) Intermediate Front End Development Projects/Build a page using Twitch.tv API/js/script.js
+++ Front End Development Certification/9) Intermediate Front End Development Projects/Build a page using Twitch.tv API/js/script.js
@@ -1,1 +1,4 @@
-var streamer = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+// Online   <i class="fas fa-user animated pulse infinite status-on"></i>
+// Offline  <i class="far fa-user status-off"></i>
+
+var theStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
